8f1bbd6848b103c9b2b8656d2d5ed879816b29a0
Charlie Hulcher <charlie.hulcher@gmail.com>
2016-08-26 16:21:24 -0400

    Allow NativeModules to fail gracefully

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const { NativeModules } = require('react-native');
 const { RNI18n } = NativeModules;
 let I18n = require('./vendor/i18n');
 
-I18n.locale = (RNI18n) ? RNI18n.locale.replace(/_/, '-') : '';
+I18n.locale = (RNI18n && typeof RNI18n !== 'undefined') ? RNI18n.locale.replace(/_/, '-') : '';
+if (typeof navigator !== 'undefined') {
+  const tries = [
+    'language',
+    'browserLanguage',
+    'systemLanguage',
+    'userLanguage',
+  ];
+  for (const i in tries) {
+    if (navigator[tries[i]]) {
+      I18n.locale = navigator[tries[i]];
+      break;
+    }
+  }
+}
 
 module.exports = I18n;
